ca3888995e7f14b94c7ad828c7aa987844de0097
zaid-builds <98413953+diaz3z@users.noreply.github.com>
2025-12-12 18:21:47 +0530

    Update python-stage0.py

--- python-stage0.py
+++ python-stage0.py
@@ -61,13 +61,13 @@
 
 # Right sided Triangle
 
-num = 5
-for i in range(num):
-    for j in range(i+1):
-        print(" ", end=" ")
-    for j in range(i, num):
-        print("*", end=" ")
-    print()
+# num = 5
+# for i in range(num):
+#     for j in range(i+1):
+#         print(" ", end=" ")
+#     for j in range(i, num):
+#         print("*", end=" ")
+#     print()
 
 #   * * * * * 
 #     * * * * 
