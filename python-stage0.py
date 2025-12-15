95dd669ea058aae18d9ce5200acd8dde6180fd4a
zaid-builds <98413953+diaz3z@users.noreply.github.com>
2025-12-15 10:42:46 +0530

    Update python-stage0.py

--- python-stage0.py
+++ python-stage0.py
@@ -15,6 +15,37 @@
 #         print()
 # hollowsquare(5, 10)
 
+#Increasing Triangle
+
+# n = 5
+# for i in range(n):
+#     for j in range(i +1):
+#         print("*", end=" ")
+#     print()
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+
+
+
+#Decreasing Triangle
+# n = 5
+# for i in range(n):
+#     for j in range(i, n):
+#         print("*", end=" ")
+#     print()
+
+# * * * * * 
+# * * * * 
+# * * * 
+# * *
+# *
+
+
+
 
 
 
