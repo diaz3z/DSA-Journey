6652099bd2b93a9553437fbb0ce6745fe6b50611
zaid-builds <98413953+diaz3z@users.noreply.github.com>
2025-12-15 10:58:42 +0530

    Update python-stage0.py

--- python-stage0.py
+++ python-stage0.py
@@ -125,6 +125,24 @@
 
 # Inverted Pyramid
 
+# n =5
+# for i in range(n):
+#     for j in range(i +1):
+#         print(" ", end=" ")
+#     for j in range(i, n-1):
+#         print("*", end=" ")
+#     for j in range(i, n):
+#         print("*", end=" ")
+#     print()
+
+#   * * * * * * * * * 
+#     * * * * * * * 
+#       * * * * * 
+#         * * * 
+#           *
+
+
+
 # Diamond Pattern
 
 # Number Pyramid
