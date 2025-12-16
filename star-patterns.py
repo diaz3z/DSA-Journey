a8c947f341de833a4afdb24782232472a5bf17af
zaid-builds <98413953+diaz3z@users.noreply.github.com>
2025-12-16 18:56:39 +0530

    Update star-patterns.py

--- star-patterns.py
+++ star-patterns.py
@@ -16,18 +16,86 @@
 #     print()
 
 
-num = 5
+# num = 5
 
-for i in range(num):
-    for j in range(i + 1):
-        if i % 2 == 0:
-            print("A", end=" ")
-        else:
-            print("B", end=" ")
-    print()
-
-
+# for i in range(num):
+#     for j in range(i + 1):
+#         if i % 2 == 0:
+#             print("A", end=" ")
+#         else:
+#             print("B", end=" ")
+#     print()
 
 
+# num = 5
+# for i in range(num):
+#     for j in range(i+1):
+#         print(" ", end=" ")
+#     for j in range(i, num):
+#         print("*", end=" ")
+#     print()
 
 
+# num = 5
+# p = 1
+# for i in range(num):
+#     for j in range(i + 1):
+#         print(p, end=" ")
+#         p += 1
+#     print()
+
+
+
+# num = 5
+# for i in range(num):
+#     for j in range(i + 1):
+#         if (i + j) % 2 == 0:
+#             print("1", end=" ")
+#         else:
+#             print("0", end=" ")
+#     print()
+
+
+
+# num = 5
+# for i in range(num-1):
+#     for j in range(i + 1):
+#         print("*", end=" ")
+#     for j in range(i, num-1):
+#         print(" ", end=" ")
+#     for j in range(i, num-1):
+#         print(" ", end=" ")
+#     for j in range(i + 1):
+#         print("*", end=" ")
+#     print()
+# for i in range(num):
+#     for j in range(i, num):
+#         print("*", end=" ")
+#     for j in range(i ):
+#         print(" ", end=" ")
+#     for j in range(i):
+#         print(" ", end=" ")
+#     for j in range(i, num):
+#         print("*", end=" ")
+#     print()
+
+
+
+
+# num = 5
+# for i in range(num-1):
+#     for j in range(i, num):
+#         print(" ", end=" ")
+#     for j in range(i):
+#         print("*", end=" ")
+#     for j in range(i + 1):
+#         print("*", end=" ")
+#     print()
+# for i in range(num):
+#     for j in range(i + 1):
+#         print(" ", end=" ")
+#     for j in range(i, num-1):
+#         print("*", end=" ")
+#     for j in range(i, num):
+#         print("*", end=" ")
+#     print()
