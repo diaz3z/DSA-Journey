9efcd457a038f73a882e078f862d2c88deb208f8
zaid-builds <98413953+diaz3z@users.noreply.github.com>
2025-12-16 11:59:18 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -20,15 +20,50 @@
 # print(p.show_salary())
 
 
-class Employee:
-    def __init__(self):
-        self.name = "John"
-        self._department = "HR"
-        self.__salary = 60000
+# class Employee:
+#     def __init__(self):
+#         self.name = "John"
+#         self._department = "HR"
+#         self.__salary = 60000
     
-    def get_salary(self):
-        return self.__salary
-emp = Employee()
-print(emp.name)            # Public access
-print(emp._department)     # Protected access (not recommended)
-print(emp.get_salary())    # Accessing private variable via method
+#     def get_salary(self):
+#         return self.__salary
+# emp = Employee()
+# print(emp.name)            # Public access
+# print(emp._department)     # Protected access (not recommended)
+# print(emp.get_salary())    # Accessing private variable via method
+
+
+# Getters and Setters
+# Concept, Used to control access to private variables.
+
+# class BankAccount:
+#     def __init__(self, balance):
+#         self.__balance = balance
+    
+#     @property
+#     def get_balance(self):
+#         self.__balance
+
+#     @get_balance.setter
+#     def set_balance(self, __balance):
+#         if __balance < 0:
+#             print("Value is negative cannot process!!")
+#         else:
+#             print(__balance)
+
+# amount = BankAccount(1000)
+# print(amount.get_balance)
+# amount.set_balance = -154
+# # print(amount.set_balance)
+
+
+
+
+
+
+
+
+
+
+
