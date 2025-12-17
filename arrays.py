11a8d9d6f57630fdc9d8601bf39cef6b8fd20e21
zaid-builds <98413953+diaz3z@users.noreply.github.com>
2025-12-17 12:08:15 +0530

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,25 @@
+# expense = [2200, 2350, 2600, 2130, 2190]
+# extra = expense[1] - expense[0]
+# print(f"Extra expense in Fabruary: {extra}")
+# print()
+
+# total_quater = expense[0] + expense[1] + expense[2]
+# print(f"Total expense in first quater: {total_quater}")
+# print()
+
+# for i in expense:
+#     if i == 2000:
+#         print(f"Spend 2000 in {i}")
+#     else:
+#         print(f"Doesn't spend exactly 2000 in any month")
+
+# print()
+
+# june_expense = 1980
+# expense.append(june_expense)
+# print(f"updated Expense list after june: {expense}")
+# print()
+
+# refund = 200
+# expense[3] = expense[3] + refund
+# print(f"updated list after refund { expense}")
