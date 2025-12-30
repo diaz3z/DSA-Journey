d3ac61d334f301f7d4d2c90dc843156b39190a6c
zaid-builds <98413953+diaz3z@users.noreply.github.com>
2025-12-30 13:44:44 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -14,6 +14,6 @@ def binary_search(arr, target):
             return -1
       
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-target = 5
+target = 10
 result = binary_search(arr, target)
 print(f"Element found at index: {result}" if result != -1 else "Element not found in array")
