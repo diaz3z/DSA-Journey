aa6c5918723ebbef5cafd6bc5eea1bf6e8b5517e
zaid-builds <98413953+diaz3z@users.noreply.github.com>
2026-02-16 17:00:32 +0530

    Create search-insert-position.py

---
+++ Blind 75/search-insert-position.py
@@ -0,0 +1,39 @@
+"""
+Given a sorted array of distinct integers and a target value, return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+"""
+
+nums = [1,3,5,6]
+target = 2
+
+def inserPosition(nums, target):
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        mid = (l + r) // 2
+
+        if nums[mid] == target:
+            print(f"Element found at index {mid}")
+            return mid
+        elif nums[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    print(f"element should be palced at {l} index")
+    return l 
+
+
+inserPosition(nums, target)
+    
+    
